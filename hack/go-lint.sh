b8aab53453051e10b83cb04d9250e6f72a7adea7
Christophe Fergeau <cfergeau@redhat.com>
2020-05-19 23:02:52 +0200

    Use podman instead of docker
    This can be overridden by setting the RUNTIME environment variable

--- hack/go-lint.sh
+++ hack/go-lint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Example:  ./hack/go-lint.sh installer/... pkg/... tests/smoke
 
+RUNTIME=${RUNTIME:-podman}
+
 if [ "$IS_CONTAINER" != "" ]; then
   golint -set_exit_status "${@}"
 else
-  docker run --rm \
+  "$RUNTIME" run --rm \
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/openshift/cluster-api-provider-libvirt:z" \
     --workdir /go/src/github.com/openshift/cluster-api-provider-libvirt \
--- hack/go-vet.sh
+++ hack/go-vet.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+
+RUNTIME=${RUNTIME:-podman}
+
 if [ "$IS_CONTAINER" != "" ]; then
   go vet "${@}"
 else
-  docker run --rm \
+  "$RUNTIME" run --rm \
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/openshift/cluster-api-provider-libvirt:z" \
     --workdir /go/src/github.com/openshift/cluster-api-provider-libvirt \
